da36b027166a06e9f9185c1dac0bcc39309e71ae
harukitosa <13haruki28@gmail.com>
2021-04-03 15:26:56 +0900

    fix

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log("hoge");
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function encode(err) {
+    return {
+        message: err.message,
+        name: err.name,
+        stack: err.stack
+    };
+}
+function decode(err) {
+    var newError = Error();
+    newError.message = err.message;
+    newError.name = err.name;
+    newError.stack = err.stack;
+    return newError;
+}
+
+exports.decode = decode;
+exports.encode = encode;
